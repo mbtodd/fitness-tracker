99fd55710c8c7bf30400e9b903d0680844b3eed8
mbtodd <michaelbtodd@yahoo.com>
2019-09-02 15:03:14 -0400

    Routing and app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { NewComponent } from './training/new/new.component';
-import { PastComponent } from './training/past/past.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -20,10 +22,9 @@ import { AppRoutingModule } from './app-routing.module';
     LoginComponent,
     TrainingComponent,
     CurrentTrainingComponent,
-    NewComponent,
-    PastComponent,
-    WelcomeComponent,
-    AppRoutingModule
+    NewTrainingComponent,
+    PastTrainingsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -11,4 +10,4 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  .catch(err => console.log(err));
