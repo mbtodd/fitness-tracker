a1a3fe0579c97d80bbf52df0be1b579feb63f1ea
mbtodd <michaelbtodd@yahoo.com>
2019-09-18 22:47:55 -0400

    sidenav start

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,7 +8,8 @@ import {
   MatNativeDateModule,
   MatCheckboxModule,
   MatSelectModule,
-  MatOptionModule
+  MatOptionModule,
+  MatSidenavModule
 } from '@angular/material';
 
 @NgModule({
@@ -21,7 +22,8 @@ import {
     MatNativeDateModule,
     MatCheckboxModule,
     MatSelectModule,
-    MatOptionModule
+    MatOptionModule,
+    MatSidenavModule
   ],
   exports: [
     MatButtonModule,
@@ -32,7 +34,8 @@ import {
     MatNativeDateModule,
     MatCheckboxModule,
     MatSelectModule,
-    MatOptionModule
+    MatOptionModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule {}
